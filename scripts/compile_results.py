d9303a150a928a88a1e838587e56ff9d2d7a63d9
Brayden Goldstein-Gelb <brayden.goldstein-gelb@newgridinc.com>
2025-05-30 19:26:17 -0400

    Can now compute mean/std of fidelities computed by HPC

---
+++ scripts/compile_results.py
@@ -0,0 +1,21 @@
+import argparse
+import numpy as np
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--output_file_prefix", type=str)
+    n_files = 150
+
+    args = parser.parse_args()
+    fids = []
+    for idx in range(n_files):
+        filename = f'{args.output_file_prefix}_output_{idx}.txt'
+        with open(filename) as file:
+            fids.append(int(file.read()))
+
+    mean = np.mean(fids)
+    std = np.std(fids)
+    print(mean, std)
+
+if __name__ == "__main__":
+    main()
