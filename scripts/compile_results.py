74fe3d0a0c2b187bea20f50c3c98f359bc9d9e6b
Brayden Goldstein-Gelb <brayden.goldstein-gelb@newgridinc.com>
2025-05-30 21:27:23 -0400

    Streamlined getting results

--- scripts/compile_results.py
+++ scripts/compile_results.py
@@ -1,16 +1,22 @@
 import argparse
 import numpy as np
+import os
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument("--output_file_prefix", type=str)
+    parser.add_argument("--output_folder", type=str)
     n_files = 150
 
     args = parser.parse_args()
     fids = []
-    for idx in range(n_files):
-        filename = f'{args.output_file_prefix}_{idx}.txt'
-        with open(filename) as file:
+    filenames = os.listdir()
+    filenames = [f for f in filenames if '.txt' in f]
+    if len(filenames) < 150:
+        print('Must have >= 150 files')
+
+    for filename in filenames:
+        path = f'{args.output_folder}/{filename}'
+        with open(path) as file:
             fids.append(float(file.read()))
 
     mean = np.mean(fids)
