db9216aa013012f24f5b3f8d9e221c5ad8ee59fb
Brayden Goldstein-Gelb <brayden.goldstein-gelb@newgridinc.com>
2025-06-02 00:09:23 -0400

    Added data and graphs

--- scripts/generate_graph.py
+++ scripts/generate_graph.py
@@ -9,8 +9,8 @@ def fidelity_model(n, a, b):
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument("--csv_path", type=str, default='../data/nisq/cswap/n_trgts=1,2,3,4,5,6-p2=0.001,0.003,0.005-method=teledata.csv')
-    parser.add_argument("--save_path", type=str, default='../data/nisq/cswap_graphs/graph.pdf')
+    parser.add_argument("--csv_path", type=str, default='../data/nisq/cswap/n_trgts=1,2,3,4,5,6-p2=0.001,0.003,0.005-method=telegate.csv')
+    parser.add_argument("--save_path", type=str, default='../data/nisq/cswap_graphs/telegate_simulations.pdf')
 
     args = parser.parse_args()
     df = pd.read_csv(args.csv_path)
@@ -37,7 +37,7 @@ def main():
         # plot the model
         n_fit = np.linspace(min(n_vals), max(n_vals), 100)
         fid_fit = fidelity_model(n_fit, *p_opt)
-
+        print(p_opt)
         # only add fit label the first time
         if idx == 0:
             plt.plot(n_fit, fid_fit, linestyle='--', label='Fit', c=colors[idx], alpha=0.6)
@@ -47,7 +47,7 @@ def main():
 
     plt.xlabel(r'Number of Targets ($n$)', fontsize=12)
     plt.ylabel('Fidelity', fontsize=12)
-    plt.title('Mean Fidelity of Two-Party CSWAP (Teledata Scheme)', fontsize=13)
+    plt.title('Mean Fidelity of Two-Party CSWAP (Telegate Scheme)', fontsize=13)
     plt.ylim(0.85, 1.01)
     plt.xticks(df['n_trgts'].unique())
     plt.legend(fontsize=10)
