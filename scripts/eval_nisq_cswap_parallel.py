a4522d0e39af12c6a37ad5e4ebfceb55865cb8f7
kunliu7 <952235037@qq.com>
2025-07-24 14:20:30 -0400

    create folders automatically

--- scripts/eval_nisq_cswap_parallel.py
+++ scripts/eval_nisq_cswap_parallel.py
@@ -1,4 +1,5 @@
 import argparse
+import os
 import time
 
 from dqalgo.nisq.eval_cswap import (eval_CSWAP_teledata_single_thread,
@@ -15,10 +16,14 @@ def main():
     parser.add_argument("--iters_per_input", type=int, default=1)
     parser.add_argument("--method", type=str, default="telegate", choices=["teledata", "telegate"])
     parser.add_argument("--slurm_index", type=int)
-    parser.add_argument("--output_file_prefix", type=str)
+    # parser.add_argument("--output_file_prefix", type=str)
 
     args = parser.parse_args()
     print(args)
+    output_dir = f'./data/nisq/cswap/{args.method}-t={args.n_trgts}-p={args.p2}'
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+    
     if args.method == "teledata":
         print("Using teledata method")
         eval_func = eval_CSWAP_teledata_single_thread
@@ -33,7 +38,7 @@ def main():
         shots_per_circ=args.n_shots)
     time_end = time.time()
 
-    with open(f'{args.output_file_prefix}_{args.slurm_index}.txt', 'w') as f_out:
+    with open(f'{output_dir}/{args.slurm_index}.txt', 'w') as f_out:
         f_out.write(str(fid))
 
     print(f"Time taken: {time_end - time_start} seconds")
