40855d75887911fdd4279f7e646ca134b1f9dfbc
kunliu7 <952235037@qq.com>
2025-07-24 15:09:54 -0400

    recover slurm index

--- scripts/eval_nisq_cswap_parallel.py
+++ scripts/eval_nisq_cswap_parallel.py
@@ -15,15 +15,9 @@ def main():
     parser.add_argument("--n_shots", type=int, default=1024)
     parser.add_argument("--iters_per_input", type=int, default=1)
     parser.add_argument("--method", type=str, default="telegate", choices=["teledata", "telegate"])
-    # parser.add_argument("--slurm_index", type=int)
-
-    # parser.add_argument("--output_file_prefix", type=str)
+    parser.add_argument("--slurm_index", type=int)
 
     args = parser.parse_args()
-    my_slurm_index = os.getenv('SLURM_ARRAY_TASK_ID')
-    assert my_slurm_index is not None, "SLURM_ARRAY_TASK_ID is not set"
-
-    print(args)
     output_dir = f'./data/nisq/cswap/{args.method}-t={args.n_trgts}-p={args.p2}'
     if not os.path.exists(output_dir):
         os.makedirs(output_dir)
@@ -42,7 +36,7 @@ def main():
         shots_per_circ=args.n_shots)
     time_end = time.time()
 
-    with open(f'{output_dir}/{my_slurm_index}.txt', 'w') as f_out:
+    with open(f'{output_dir}/{args.slurm_index}.txt', 'w') as f_out:
         f_out.write(str(fid))
 
     print(f"Time taken: {time_end - time_start} seconds")
