18fa6fa0aee9519eb93cd6b8dff7f1525d094a0e
kunliu7 <952235037@qq.com>
2025-07-24 13:34:47 -0400

    bash for Yale HPC

---
+++ slurm_script_Yale.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#SBATCH --job-name=eval_nisq_cswap
+#SBATCH --output=logs/eval_nisq_cswap_%j.out
+#SBATCH --error=logs/eval_nisq_cswap_%j.err
+#SBATCH --time=02:00:00
+#SBATCH --partition=standard
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=4
+#SBATCH --array=0-149
+#SBATCH --mem=8G
+#SBATCH --mail-type=ALL
+
+
+module load miniconda
+conda activate qlop
+
+python scripts/eval_nisq_cswap_parallel.py "$@"
