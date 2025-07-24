2f8b316f6460c618f45b6cb0cf0b23805cea6ee3
kunliu7 <952235037@qq.com>
2025-07-24 15:14:22 -0400

    all the bashes

---
+++ bashes/yale-telegate-3-0.003.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#SBATCH --job-name=eval_nisq_cswap
+#SBATCH --output=logs/eval_nisq_cswap_%j.out
+#SBATCH --error=logs/eval_nisq_cswap_%j.err
+#SBATCH --time=02:00:00
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=4
+#SBATCH --array=0-149
+#SBATCH --mail-type=ALL
+
+module load miniconda
+conda activate dqalgo
+python scripts/eval_nisq_cswap_parallel.py --n_trgts 3 --p2 0.003 --method telegate --slurm_index ${SLURM_ARRAY_TASK_ID}
