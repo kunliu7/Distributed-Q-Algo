de1fb0416cc7ae16dcae8a6d0f39abace3d12cac
kunliu7 <952235037@qq.com>
2025-12-12 09:36:17 -0500

    fix datapath name

--- scripts/eval_nisq_ghz_prep.py
+++ scripts/eval_nisq_ghz_prep.py
@@ -7,6 +7,7 @@ import pandas as pd
 from dqalgo.data_mgr import NISQPrepareGHZDataMgr
 from dqalgo.nisq.eval import eval_GHZ_prep
 
+
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("--n_parties", "-t", type=int, nargs="+", default=[4])
@@ -16,8 +17,8 @@ def main():
 
     dmgr = NISQPrepareGHZDataMgr()
     fid_data = []
+    time_start = time.time()
     for n_parties, p2 in itertools.product(args.n_parties, args.p2):
-        time_start = time.time()
         p1 = p2 / 10
         pm = p2
         print(f"n_parties: {n_parties}, p1: {p1}, p2: {p2}, pm: {pm}")
@@ -26,7 +27,7 @@ def main():
         fid_data.append([n_parties, p2, fid])
 
     out_df = pd.DataFrame(fid_data, columns=["n_parties", "p2", "fid"])
-    dmgr.save(out_df, n_trgts=args.n_parties, p2=args.p2)
+    dmgr.save(out_df, n_parties=args.n_parties, p2=args.p2)
     time_end = time.time()
     print(f"Time taken: {time_end - time_start} seconds")
 
